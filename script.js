809fa2f69569b8174dbf4c0346dcc6013a253150
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-03-07 21:14:23 -0700

    functionality working on shopping cart as well as only allowing for a total of 10 items in cart, still need to work out the styling

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ let updateItemTotal = function() {
     
     // getting total cost per item
     let itemTotal = costPerItem * quanity; 
-    //console.log(itemTotal);
-    
-    // get cart total
-    
     
     // injecting hmtl into the total column
     $(ele).children('.total').html(itemTotal);
@@ -24,27 +20,65 @@ let updateItemTotal = function() {
 };
 
 let grandTotals = [];
-var cartTotal = 0;  
+var cartTotal = 0;
+
+let sum = function (acc, x) { return acc + x;};
+
 let updateGrandTotal = function() {
+  grandTotals.splice(0, grandTotals.length);
   $('tbody tr').each(function(i, ele) {
-    grandTotals.splice(0, grandTotals.length);
     let total = parseFloat($(ele).children('.total').text());
     grandTotals.push(total);
-    console.log(grandTotals);
+    //console.log(grandTotals);
     
     cartTotal = grandTotals.reduce(sum);
-    console.log(cartTotal);
+    //console.log(cartTotal);
     return cartTotal;
   });
   $('#cart-Total').text(`Cart Total: $ ${cartTotal}`);
 };
 
+let checkForMaxItems = function() {
+  let maxItems = 3;
+  let currentItems = $('tbody .item').length;
+  //console.log(currentItems);
+  if (currentItems >= maxItems) {
+    $('#btn-add').hide();
+  } else if (currentItems < maxItems) {
+    $('#btn-add').show();
+  }
+};
 
-let sum = function (acc, x) { return acc + x;};
+$(document).on('click', '#btn-delete', function() {
+  $(this).closest('tr').remove();
+  updateItemTotal();
+  updateGrandTotal();
+  checkForMaxItems();
+});
+
+$('#btn-add').on('submit', function(event) {
+  event.preventDefault();
+  let newItemName = $(this).children('[name=name-of-item]').val();
+  let newItemCost = $(this).children('[name=cost-of-item]').val();
+  //console.log(newItemName, newItemCost);
+  
+  
+  $('tbody').append('<tr>' + 
+  '<td class="item">' + newItemName + '</td>' + 
+  '<td class="cost">' + newItemCost + '</td>' + 
+  '<td>' + '<input class="qty" type="number" value="1">' + '</td>' + 
+  '<td class="total">' + '</td>' + 
+  '<td class="btn-delete">' + '<button id="btn-delete">Delete</button>' + '</td>' + '</tr>');
+  updateItemTotal();
+  updateGrandTotal();
+  $(this).children('[name=name-of-item]').val('');
+  $(this).children('[name=cost-of-item]').val('');
+  checkForMaxItems();
+});
 
 $(document).ready(function() {
   // Your code here
-  console.log(cartTotal);
+  //console.log(cartTotal);
   updateItemTotal();
   updateGrandTotal();
   $('.qty').on('input', function() {
